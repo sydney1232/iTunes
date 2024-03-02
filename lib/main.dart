dc61b869b38376abbf2e347e1d2180f0a4cf1c73
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-03-02 13:10:19 +0800

    Convert api data result to local date

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:itunes_api/model/itunes_model.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -15,16 +18,30 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+List<Song> songResult = [];
+
 Future getSearch() async {
   const String apiUrl = 'https://itunes.apple.com/search?term=jack+johnson';
   final http.Response response = await http.get(Uri.parse(apiUrl));
-  print(response.body);
+  parseJson(response);
+}
+
+parseJson(http.Response response) {
+  var jsonData = jsonDecode(response.body);
+
+  for (var eachResult in jsonData['results']) {
+    final result = Song(
+      artistName: eachResult['artistName'],
+      trackName: eachResult['trackName'],
+    );
+    songResult.add(result);
+  }
 }
 
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     getSearch();
-    return const Scaffold();
+    return Scaffold();
   }
 }
