c075653b50eedea4453ce1eb508aff47306aa96f
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-03-02 12:28:43 +0800

    Added getSearch function for api request

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MaterialApp(
@@ -14,9 +15,16 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+Future getSearch() async {
+  const String apiUrl = 'https://itunes.apple.com/search?term=jack+johnson';
+  final http.Response response = await http.get(Uri.parse(apiUrl));
+  print(response.body);
+}
+
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    getSearch();
     return const Scaffold();
   }
 }
