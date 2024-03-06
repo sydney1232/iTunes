c01d6882a20a5595cda4129bd5265d96506c16b8
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-03-06 22:59:10 +0800

    Beautify Card title and added additional data

--- lib/widgets/Card.dart
+++ lib/widgets/Card.dart
@@ -6,11 +6,17 @@ class CardTile extends StatefulWidget {
   final String title;
   final String subtitle;
   final String webUrl;
+  final String genre;
+  final String currency;
+  final double trackPricing;
   const CardTile(
       {super.key,
       required this.title,
       required this.subtitle,
-      required this.webUrl});
+      required this.webUrl,
+      required this.genre,
+      required this.currency,
+      required this.trackPricing});
 
   @override
   State<CardTile> createState() => _CardTileState();
@@ -20,44 +26,67 @@ class _CardTileState extends State<CardTile> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(23),
-      child: Column(
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      padding: EdgeInsets.all(10),
+      child: Card(
+        elevation: 4,
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
             children: [
-              Image.network(
-                fit: BoxFit.fill,
-                widget.webUrl,
-                height: 150,
-                width: 100,
-              ),
-              SizedBox(width: 10),
-              Expanded(
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.title,
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 15),
-                      Text(
-                        widget.subtitle,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.grey),
-                      ),
-                    ],
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      fit: BoxFit.fill,
+                      widget.webUrl,
+                      height: 160,
+                      width: 100,
+                    ),
                   ),
-                ),
-              ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.title,
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            widget.subtitle,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                          ),
+                          Text(
+                            widget.genre,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '${widget.currency.toString()} ${widget.trackPricing.toString()}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
