a1e1b908d02b7459d5661e9c96693257382bd1e5
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-03-06 22:57:58 +0800

    Added genre and currency data in model

--- lib/model/itunes_model.dart
+++ lib/model/itunes_model.dart
@@ -2,9 +2,16 @@ class Song {
   final String artistName;
   final String trackName;
   final String artworkUrl60;
+  final String genre;
+  final String currency;
+  final double trackPricing;
 
-  Song(
-      {required this.artistName,
-      required this.trackName,
-      required this.artworkUrl60});
+  Song({
+    required this.artistName,
+    required this.trackName,
+    required this.artworkUrl60,
+    required this.genre,
+    required this.currency,
+    required this.trackPricing,
+  });
 }
